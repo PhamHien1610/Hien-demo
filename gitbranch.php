7df950180773c902604ddad5441baaecaef576d4
Hien <phamhien16102003@gmail.com>
2023-11-04 14:28:49 +0700

    Add file gitbranch.php

---
+++ gitbranch.php
@@ -0,0 +1,9 @@
+<html>
+<head> </head>
+<body>
+    <?php
+    echo "Các Branch (nhánh) đại diện cho các phiên bản cụ thể của một kho lưu trữ tách ra từ project chính của bạn. <br/>";
+    echo "Branch cho phép bạn theo dõi các thay đổi thử nghiệm bạn thực hiện đối với kho lưu trữ và có thể hoàn nguyên về các phiên bản cũ hơn.";
+    ?>
+</body>
+</html>
